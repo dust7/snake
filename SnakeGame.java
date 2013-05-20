7d6111ad79516245407d6bae2bd15239b5b63f0f
unknown <dustyyyy@web.de>
2013-05-20 22:17:34 +0200

    minor changes, fixed exception

--- SnakeGame.java
+++ SnakeGame.java
@@ -6,7 +6,7 @@ class SnakeGame {
     private static Snake snake; //statics used by WindowListener methods in SnakeFrame
     private static Food food;
     private static Grid grid;
-    private static int time=400; //time between moves
+    private static int time=200; //time between moves
 
     public static Snake getSnake() {
         return new Snake(snake);
@@ -20,14 +20,12 @@ class SnakeGame {
         return new Grid(grid);
     }
     
-    private static void adjustTime() {
-        if(time>300) {
-            time=time-20;
-        } else if(time<=300 && time>200) {
-            time=time-3;
-        } else if(time<=200 && time>150) {
+    private static void adjustTime() {    
+        if(time>150) {
+            time=time-4;        
+        } else if(time<=150 && time>100) {
             time=time-2;
-        } else if(time<=150 && time>20) {
+        } else if(time<=100 && time>20) {
             time=time-1;
         }    
     }
@@ -70,24 +68,15 @@ class SnakeGame {
         }
         askuser.setVisible(false);
         askuser.dispose();
-        //init playzone
-        SnakeFrame frame=new SnakeFrame(askuser.getUserX(), askuser.getUserY());            
+        //init grid, snake, food, frame                           
         grid=new Grid(askuser.getUserX(), askuser.getUserY());        
-        //init snake, food   
         snake=new Snake(grid);            
         food=new Food(grid);
         while(food.isOnSnake(snake)) {
             food.jump(grid);
-        }
-        //paint grid, head, food
-        try { //give frame time to load, prevent painting bugs
-            Thread.sleep(100);
-        } catch(InterruptedException ex) {}
-        ((SnakeFrame)frame).setBg(new Color(244, 242, 255)); //almost white
-        SnakePainter.paintDeadzone(frame,grid);
-        SnakePainter.paintPlayzone(frame,grid);
-        SnakePainter.repaintSnake(frame,snake,food);    
-        SnakePainter.repaintFood(frame,food);          
+        }       
+        SnakeFrame frame=new SnakeFrame(askuser.getUserX(), askuser.getUserY());
+        ((SnakeFrame)frame).setBg(new Color(228, 228, 228));          
         //start game        
         inform();
         System.out.println("use arrow keys to move!");        
@@ -124,7 +113,7 @@ class SnakeGame {
                 inform();
                 }    
             } else {
-            SnakePainter.repaintSnake(frame,snake,food);
+                SnakePainter.repaintSnake(frame,snake,food);
             }
             //limit gamespeed
             try {
